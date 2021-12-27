fa376ad796150243566a7f3af1f0cf18abe7a7aa
duxmazter <46duxmazter@gmail.com>
2021-12-27 15:15:00 +0700

    fin

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,39 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double d[],int N,double sV[]){
+    
+    double AM, sum = 0;
+    for(int i = 0; i < N; i++){
+        sum += d[i];
+    } AM = sum / N;
+    
+    double SD; sum = 0;
+    for(int i = 0; i < N; i++){
+        sum += pow(d[i]-AM,2);
+    } SD = sqrt(sum/N);
+    
+    double GM; sum = 1;
+    for(int i = 0; i < N; i++){
+        sum *= d[i];
+    } GM = pow(sum,(double)1/N);
+
+    double HM; sum = 0;
+    for(int i = 0; i < N; i++){
+        sum += 1.0/d[i];
+    } HM = N / sum;
+
+    double max = d[0] , min = d[0];
+    for (int i = 0;i < N; i++){
+        if(d[i]>max) max = d[i];
+        if(d[i]<min) min = d[i];
+    }
+
+    sV[0] = AM;
+    sV[1] = SD;
+    sV[2] = GM;
+    sV[3] = HM;
+    sV[4] = max;
+    sV[5] = min;
+}
