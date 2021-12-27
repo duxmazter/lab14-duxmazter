75a2da3bd0d300fff461f4be5a943aa54f87928c
duxmazter <46duxmazter@gmail.com>
2021-12-27 18:07:15 +0700

    fin

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,29 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[][M]){
+    for(int i = 0; i < 72 ; i++)cout <<"-"; 
+    cout <<endl;
+    for(int Row = 0; Row < N ; Row++){
+        cout <<"|";
+        for(int i = 0; i < M ; i++){
+            if(image[Row][i] == 0) cout <<" ";
+            else cout <<"*";
+        }
+        cout <<"|"<<endl;
+    }
+    for(int i = 0; i < 72 ; i++ )cout <<"-";
+    cout <<endl;
+}
+
+void updateImage(bool image[][M],int s,int x,int y){
+    int i, j;
+    for(i = 0; i < N ; i++){
+        for(j = 0; j < M ; j++){
+            if (sqrt(pow(i-x,2) + pow(j-y,2)) <= s-1){
+            image[i][j] = 1;
+            }
+        }
+    }
+}
